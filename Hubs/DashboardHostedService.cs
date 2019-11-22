24ba6e42ea68521924cb6d80c15c24fc9a999a28
Nithya.M <nithya.manivannan@ameexusa.com>
2019-11-22 19:16:40 +0530

    new

---
+++ Hubs/DashboardHostedService.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SignalR.Hubs
+{
+    public class DashboardHostedService: IHostedService
+    {
+        private Timer _timer;
+       
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public DashboardHostedService(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(DoWork, null, TimeSpan.Zero,
+            TimeSpan.FromSeconds(2));
+
+            return Task.CompletedTask;
+        }
+        private string getRandomString()
+
+        {
+
+            Random random = new Random();
+
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+            return new string(Enumerable.Repeat(chars, random.Next(10, 16))
+
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+
+        }
+        private void DoWork(object state)
+        {
+            _hubContext.Clients.All.SendAsync("SendMessage",
+                new
+                {
+                    val1 =   getRandomString(),
+                    val2 = getRandomString(),
+                    val3 = getRandomString(),
+                    val4 = getRandomString()
+                });
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ controller.js
@@ -0,0 +1,50 @@
+﻿//(function () {
+//    'use strict';
+
+//    angular
+//        .module('app')
+//        .controller('controller', controller);
+
+//    controller.$inject = ['$location'];
+
+//    function controller($location) {
+//        /* jshint validthis:true */
+//        var vm = this;
+//        vm.title = 'controller';
+
+//        activate();
+
+//        function activate() { }
+//    }
+//})();
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Subject } from 'rxjs/Subject';
+@Injectable({
+    providedIn: 'root'
+})
+export class SignalRService {
+    private message$: Subject<message>;
+    private connection: signalR.HubConnection;
+
+    constructor() {
+        this.message$ = new Subject<message>();
+        this.connection = new signalR.HubConnectionBuilder()
+            .withUrl(environment.hubUrl)
+            .build();
+        this.connect();
+    }
+    private connect() {
+        this.connection.start().catch(err => console.log(err));
+        this.connection.on('SendMessage', (message) => {
+            this.message$.next(message);
+        });
+    }
+    public getMessage(): Observable<message> {
+        return this.message$.asObservable();
+    }
+    public disconnect() {
+        this.connection.stop();
+    }
+}
+
